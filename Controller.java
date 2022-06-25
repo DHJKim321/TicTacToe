bcb554a32c2f6e58a081aa80d3679bee164fc3e3
Daniel Kim <dhjkim2001@gmail.com>
2022-06-25 22:24:36 +0100

    Added more input methods

--- Controller.java
+++ Controller.java
@@ -5,7 +5,7 @@ public class Controller {
     }
 
     public void startGame() {
-        
+        welcome();
     }
 
 }
--- InputUtil.java
+++ InputUtil.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class InputUtil {
     
-    public InputUtil() {
+    public void welcome() {
+        System.out.println("Welcome to TicTacToe!");
+    }
 
+    public void sayTurn(int turn) {
+        if (turn == 0) {
+            System.out.println("It is Player 1's turn!");
+        }
+        else {
+            System.out.println("It is Player 2's turn!");
+        }
+    }
+
+    public void playerWon(int player) {
+        System.out.printf("Player %d has won!", player);
+    }
+
+    public int askForBoardSize() {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("How big do you want the board to be?: ");
+        int size = reader.nextInt();
+        reader.close();
+        return size;
     }
 }
