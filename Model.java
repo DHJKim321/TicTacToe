4e169226316ba82e6bca5ba4dbfa6ee61e24e515
Daniel Kim <dhjkim2001@gmail.com>
2022-06-26 17:30:16 +0100

    Added model variables

--- Model.java
+++ Model.java
@@ -2,6 +2,8 @@ public class Model {
     
     final int SIZE = 3;
     int turn = 0;
-
+    String[][] board = new String[SIZE][SIZE];
+    final String player_1 = "O";
+    final String player_2 = "X";
     
 }
